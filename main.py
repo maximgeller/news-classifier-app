08a5983eef37cf37e99c9b29fc501003638d694a
maximgeller <maxim.geller@yahoo.com>
2020-06-12 21:00:25 -0400

    started styling and aws

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = None
+
+def load_model():
+    global model
+
+    with open('nb_trained_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    if request.method == "POST":
+        data = request.get_json()
+        print(data.size)
+    return "placeholder"
+
+if __name__ == "__main__":
+    load_model()
+    app.run(debug=True)
