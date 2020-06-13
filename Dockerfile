b45b7cdc769c6028551c0a241ff2c76cf9bb484d
maximgeller <maxim.geller@yahoo.com>
2020-06-12 22:13:58 -0400

    added dockerfile, virtual environment, began ml pipeline

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.6-slim
+COPY ./main.py /deploy/
+COPY ./requirements.txt /deploy/
+COPY ./nb_trained_model.pkl /deploy/
+WORKDIR /deploy/
+RUN pip install -r requirements.txt
+EXPOSE 80
+ENTRYPOINT ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer 
 
 app = Flask(__name__)
 
 model = None
+cv = CountVectorizer()
 
 def load_model():
     global model
@@ -20,8 +22,14 @@ def home():
 def predict():
     if request.method == "POST":
         data = request.get_json()
-        print(data.size)
-    return "placeholder"
+        data = np.reshape(data, (-1, 1))
+        data = cv.transform(data)
+        prediction = model.predict([data])
+        # message = request.form['article']
+        # data = [message]
+        # vect = cv.transform(data).toarray()
+        # my_prediction = clf.predict(vect)
+        return str(prediction[0])
 
 if __name__ == "__main__":
     load_model()
