34dd892da38abd00461782c5b558f8bee8ef82a2
maximgeller <maxim.geller@yahoo.com>
2020-06-12 23:48:29 -0400

    classifier working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import pickle
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer 
+import re
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ model = None
 def load_model():
     global model
 
-    with open('nb_trained_model.pkl', 'rb') as f:
+    with open('nb_trained_model.pickle', 'rb') as f:
         model = pickle.load(f)
 
 @app.route("/")
@@ -19,14 +20,19 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-   #  cv = CountVectorizer()
+    cv = pickle.load(open("vectorizer.pickle", 'rb'))
     if request.method == "POST":
-        text = request.form['article']
-        data = {"story": [text]}
-        response = jsonify(data)
-        # vect = cv.transform(data)
-        # prediction = model.predict(vect)
-        return response
+        text = request.form.get('article')
+        text = re.sub(r"\r\n", "", text)
+        # print(text)
+        # clean_text = text.strip() 
+        data = [text]
+        # data = {"story": [text]}
+        # response = jsonify(data)
+        vect = cv.transform(data)
+        prediction = model.predict(vect)
+        return "Fake News Story" if prediction[0] else "Real News Story"
+        # return str(prediction[0])
 
 if __name__ == "__main__":
     load_model()
